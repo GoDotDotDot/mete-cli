bddaeb0f17aa2b82509c5f35fefe42421c2d5dc2
GoDotDotDot <chukui0202@gmail.com>
2018-11-25 20:14:38 +0800

    fix: can't resolve ./locale

---
+++ templates/react/mete.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const webpack = require('webpack');
+
+module.exports = {
+  webpack: config => {
+    // 解决antd moment 报 can't resolve ./locale 错误
+    config.resolve.alias = {
+      moment$: 'moment/moment.js',
+    };
+
+    return config;
+  },
+};
